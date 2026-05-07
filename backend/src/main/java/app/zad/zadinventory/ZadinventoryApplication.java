485ebdc3d1904aa186baa9329056c8c1bd52797f
Pedro Lucas <pedrolmald@gmail.com>
2026-05-07 17:01:28 -0300

    Tomcat standalone com WAR e TLS configurado no server.xml

--- backend/src/main/java/app/zad/zadinventory/ZadinventoryApplication.java
+++ backend/src/main/java/app/zad/zadinventory/ZadinventoryApplication.java
@@ -2,12 +2,18 @@ package app.zad.zadinventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ZadinventoryApplication {
+public class ZadinventoryApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZadinventoryApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZadinventoryApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(ZadinventoryApplication.class, args);
+    }
 }
